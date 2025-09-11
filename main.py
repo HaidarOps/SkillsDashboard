7228c113c4ac25a60d97168f8ef1c36d5ec25e57
Haidar <haidar@Mac.cust.communityfibre.co.uk>
2025-09-11 20:29:50 +0100

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Objective - To build a dashboard which monitors skills that i have learnt
+# LO:
+#  - Build a dashboard
+#  - Allow the user to add skills
+#  - Display skills and different Objective
+
+
+
+from PySide6.QtWidgets import QApplication, QWidget
+
+import sys
+
+app = QApplication(sys.argv)
+
+window = QWidget()
+window.show()
+
+
+app.exec()
