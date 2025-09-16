325bb60505dd7080652f9929700bf1a699638800
Haidar Kassem <haidarskassem@outlook.com>
2025-09-16 15:31:37 +0100

    Centered Icons with text

--- main.py
+++ main.py
@@ -6,14 +6,52 @@
 
 
 
-from PySide6.QtWidgets import QApplication, QWidget
-
 import sys
 
-app = QApplication(sys.argv)
+from PySide6.QtWidgets import (QLineEdit, QPushButton, QApplication, QVBoxLayout, QDialog, QLabel,QGridLayout)
+from PySide6.QtCore import Slot, Qt
+from PySide6.QtGui import QIcon,  QPixmap
 
-window = QWidget()
-window.show()
+class  Form(QDialog):
+
+    def __init__(self, parent=None):
+        super(Form, self).__init__(parent)
+        self.setWindowTitle("My Form")
+        # self.edit = QLineEdit("Write your name here... ")
+        # self.button = QPushButton("Show greetings")
+        self.icon_label = QLabel()
+        self.icon_label.setPixmap(QPixmap("./assets/book.png"))
+        self.icon_label.setFixedSize(64, 64)
+        self.icon_label.setScaledContents(True)
+        self.label_quote = QLabel("There are a thousand paths up the mountain...")
+        self.grid_layout = QGridLayout()
 
 
-app.exec()
+        # changing the layout
+        layout = QVBoxLayout(self)
+        layout.addWidget(self.icon_label, alignment = Qt.AlignCenter)
+        layout.addWidget(self.label_quote)
+        layout.setSpacing(50)
+        layout.addWidgetQGridLayout())
+        # layout.addWidget(self.edit)
+        # layout.addWidget(self.button)
+
+
+        self.setLayout(layout)
+        # self.button.clicked.connect(self.greetings)
+
+        self.setWindowIcon(QIcon("./assets/book.png"))
+
+    def greetings(self):
+        print(f"Hello {self.edit.text()}")
+
+
+
+
+#This will skip everything inside if the file is not directly called and not import
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    form = Form()
+    form.show()
+    sys.exit(app.exec())
+
